665158789eee4ae786384211f0221a65b5e1130f
solayman-bd <mdsolaymannuhin@gmail.com>
2023-09-04 15:35:50 +0600

    depl

--- TakeAQuiz/settings.py
+++ TakeAQuiz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a)+1^3tz&a2@plblj$!v39pfpi7h!b-v#e%g6-qbizt-fdmm-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
